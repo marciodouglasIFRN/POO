37cfb985694e9b06dffc76e26746f40d6eae444f
Juan Silva <jjptacima01@gmail.com>
2017-11-10 16:35:56 -0300

    criação da classe Assento

---
+++ ReservaDeAssento/src/aviao/Assento.java
@@ -0,0 +1,23 @@
+package aviao;
+
+public class Assento {
+	int fileira;
+	int coluna;
+	double valor;
+	boolean status;
+	
+
+	void marcarAssento() {
+			
+	}
+	void desmarcarAssento() {
+		
+	}
+	void remarcarAssento() {
+		
+	}
+	void assentoMarcado() {
+		
+	}
+
+}
--- ReservaDeAssento/src/aviao/Principal.java
+++ ReservaDeAssento/src/aviao/Principal.java
@@ -1,4 +1,6 @@
+package aviao;
 
 public class Principal {
 
+	
 }
